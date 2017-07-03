fbd7082c41efbce2dd18f5658b9cf441a0d1346f
Anna Horodetska and Brian Fan <anna+brian@>
2017-07-03 14:53:23 -0700

    whatever

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  has_attached_file :avatar,
-    :styles => { :medium => "300x300>", :thumb => "100x100#" },
-    :default_url => "/images/:style/missing.png"
-    :storage => :s3,
-    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
-
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-
-
-  def s3_credentials
-    {:bucket => ENV["S3_BUCKET_NAME"], :access_key_id => ENV["AWS_ACCESS_KEY_ID"],
-    :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]
-  end
 end
